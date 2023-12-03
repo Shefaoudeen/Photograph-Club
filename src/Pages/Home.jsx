ca385f87a7a5d7c8f021aea5e4fd877fac02b7da
Shefa <shefaoudeen123@gmail.com>
2023-12-03 13:57:17 +0530

    mobile

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,22 +3,24 @@ import { Logo, halfCamera } from "../assets";
 const Home = () => {
   return (
     <section className="px-5">
-      <div className="w-[100%] h-screen flex-1 flex items-center justify-around max-sm:flex-col ">
-        <div className="w-[40%] flex flex-col max-sm:text-sm">
-          <p className="text-center tracking-wide text-xl z-10">
+      <div className="w-[100%] flex-1 flex items-center justify-around max-sm:flex-col ">
+        <div className="w-[40%] flex flex-col max-sm:text-sm max-sm:w-full">
+          <p className="text-center tracking-wide text-xl z-10 max-sm:py-4">
             Puducherry Technological University's
           </p>
-          <h1 className="font-bold font-sans text-[70px] text-center drop-shadow-2xl z-10">
-            <span className="text-red-500">P</span>
-            HO
-            <span className="text-red-500">T</span>
-            OGRAPHY
-          </h1>
-          <h1 className="font-bold font-sans text-[70px] text-center z-10">
-            CL
-            <span className="text-red-500">U</span>B
-          </h1>
-          <div className="border-2 p-5 rounded-3xl z-10 drop-shadow-2xl mt-5 flex flex-col bg-gradient-to-tr from-white to-slate-100">
+          <div className="max-sm:leading-[100px]">
+            <h1 className="font-bold font-sans text-[70px] text-center drop-shadow-2xl z-10">
+              <span className="text-red-500">P</span>
+              HO
+              <span className="text-red-500">T</span>
+              OGRAPHY
+            </h1>
+            <h1 className="font-bold font-sans text-[70px] text-center z-10">
+              CL
+              <span className="text-red-500">U</span>B
+            </h1>
+          </div>
+          <div className="border-2 p-5 rounded-3xl max-sm:my-[50px] z-10 drop-shadow-2xl mt-5 flex flex-col bg-gradient-to-tr from-white to-slate-100">
             <h1 className="font-bold font-sans text-3xl">Missions</h1>
             <div className="w-full flex">
               <div className="w-[10%]"></div>
